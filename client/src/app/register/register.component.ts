583e08babb1479f2c2680408322c3a6e536a5076
Nikkula UttarKumar <qxy3281@sfap.asiapacific.bmw.corp>
2020-11-15 18:12:08 +0800

    End of section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model : any = {};
+  //@Input() usersFromHomeComponent : any;
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private accountServices : AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountServices.register(this.model).subscribe(response => {
+        console.log(response);
+        this.cancel();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
